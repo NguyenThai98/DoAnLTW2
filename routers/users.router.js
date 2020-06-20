ca5bb3b2eab2b1e8503755a5300f772c941a9b2d
NguyenThai98 <nguyenthai492018@gmail.com>
2020-06-20 23:14:10 +0700

    add list categori and chi tiet bai viet

--- routers/users.router.js
+++ routers/users.router.js
@@ -4,5 +4,10 @@ const router = express.Router();
 router.get('/login', (req, res) => {
     res.render('users/login');
 })
-
+router.get('/categories', (req, res) => {
+    res.render('users/categories');
+})
+router.get('/listCategories', (req, res) => {
+    res.render('users/listCategories');
+})
 module.exports = router;
