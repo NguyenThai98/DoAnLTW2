0c276a8f98cc6494aff21fc037981be1775f510e
NguyenThai <nguyenthai492017#gmail.com>
2020-06-23 22:44:42 +0700

    edit css

--- public/js/main.js
+++ public/js/main.js
@@ -31,28 +31,28 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 2000,
         responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
             }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
@@ -63,28 +63,28 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 2,
         responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
             }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
@@ -96,7 +96,19 @@ $(document).ready(function () {
             scrollTop: 0
         }, 1000)
     })
-    //AOS Init
+
+    //scroll menu
+    $(window).on('scroll', () => {
+        let navMenu = $('.nav');
+        var height = $(window).scrollTop();
+        if (height >= 100) {
+            $('.top-header').addClass("hide-Top-header")
+            navMenu.addClass("menuTop");
+        } else {
+            navMenu.removeClass("menuTop");
+            $('.top-header').removeClass("hide-Top-header")
+        }
+    });
 })
 
 ///js login
